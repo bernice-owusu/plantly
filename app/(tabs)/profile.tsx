1e21f019e10c0c70a347f9b82b060c1b468310e5
bernice <bernyx.owusu@gmail.com>
2025-01-08 11:27:19 +0000

    zustand setup - userStore

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,16 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
 import { theme } from "@/theme";
+import { useUserStore } from "@/store/userStore";
 
 export default function Profile() {
+  const toggleHasOnboarded = useUserStore((state) => state.toggleHasOnboarded);
+
+  const handleOnPress = () => {
+    toggleHasOnboarded();
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile</Text>
+      <Button title="Back to onboarding" onPress={handleOnPress} />
     </View>
   );
 }
@@ -15,8 +21,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
-  },
-  text: {
-    fontSize: 24,
   },
 });
--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,19 @@
+import { useUserStore } from "@/store/userStore";
 import { theme } from "@/theme";
-import { StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function Onboarding() {
+  const router = useRouter();
+  const toggleHasOnboarded = useUserStore((state) => state.toggleHasOnboarded);
+
+  const handleOnboarding = () => {
+    toggleHasOnboarded();
+    router.replace("/");
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Onboarding</Text>
+      <Button title="Let me in!" onPress={handleOnboarding} />
     </View>
   );
 }
@@ -15,8 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
-  },
-  text: {
-    fontSize: 24,
   },
 });
