c70e5ddaffa323bde36cc87f87ba1812ac91b902
bernice <bernyx.owusu@gmail.com>
2025-01-08 16:46:47 +0000

    add new plant screen

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
     color: theme.colorWhite,
     fontSize: 18,
     fontWeight: "bold",
+    textAlign: "center",
   },
   button: {
     backgroundColor: theme.colorGreen,
