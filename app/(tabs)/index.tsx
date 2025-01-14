1625981478edfafe64c8fc59cb31a05a3407b4a6
bernice <bernyx.owusu@gmail.com>
2025-01-14 12:09:15 +0000

    storing and listing plants

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { theme } from "@/theme";
+import { useRouter } from "expo-router";
+import { PlantCard } from "@/components/PlantCard";
+import { usePlantStore } from "@/store/plantStore";
+import PrimaryButton from "@/components/PrimaryButton";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const router = useRouter();
+  const plants = usePlantStore((state) => state.plants);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        contentContainerStyle={styles.contentContainer}
+        data={plants}
+        renderItem={({ item }) => <PlantCard plant={item} />}
+        ListEmptyComponent={
+          <PrimaryButton
+            title="Add your first plant"
+            onPress={() => {
+              router.navigate("/new");
+            }}
+          />
+        }
+      />
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -15,7 +34,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  contentContainer: {
+    padding: 12,
   },
 });
